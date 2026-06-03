501b04f85926d9bc8ae452361d982bff60d186d6
m-miyawaki <miyawakifamilymyam@gmail.com>
2026-06-04 01:11:35 +0900

    feat: add BaseDialog component

---
+++ src/components/dialog/BaseDialog.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-dialog v-model="model" :max-width="maxWidth">
+    <v-card>
+      <v-card-title>{{ title }}</v-card-title>
+      <v-card-text>
+        <slot />
+      </v-card-text>
+      <v-card-actions v-if="$slots.actions">
+        <slot name="actions" />
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+withDefaults(defineProps<{
+  title: string
+  maxWidth?: string
+}>(), {
+  maxWidth: '500px',
+})
+
+const model = defineModel<boolean>()
+</script>
