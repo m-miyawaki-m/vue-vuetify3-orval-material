585b9661aafca53047e3b7817e8365744351ec5c
m-miyawaki <miyawakifamilymyam@gmail.com>
2026-06-04 01:16:48 +0900

    feat: add FavoritePage and SettingsPage placeholders

---
+++ src/pages/FavoritePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <MainLayout title="お気に入り">
+    <v-container class="py-6">
+      <v-alert type="info" variant="tonal">
+        お気に入り機能は準備中です。
+      </v-alert>
+    </v-container>
+  </MainLayout>
+</template>
+
+<script setup lang="ts">
+import MainLayout from '@/components/layout/MainLayout.vue'
+</script>
