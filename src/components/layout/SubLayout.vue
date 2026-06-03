5514a667d225cd28b192b7b6d87a5de51b7f6180
m-miyawaki <miyawakifamilymyam@gmail.com>
2026-06-04 01:15:23 +0900

    feat: add SubLayout for stack navigation screens

---
+++ src/components/layout/SubLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-layout>
+    <v-app-bar color="primary" elevation="2">
+      <template #prepend>
+        <v-btn icon @click="router.back()">
+          <v-icon>mdi-arrow-left</v-icon>
+        </v-btn>
+      </template>
+      <v-app-bar-title>{{ title }}</v-app-bar-title>
+    </v-app-bar>
+    <v-main>
+      <slot />
+    </v-main>
+  </v-layout>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+defineProps<{
+  title: string
+}>()
+
+const router = useRouter()
+</script>
