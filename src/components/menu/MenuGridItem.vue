a9cbbe3b3d90a7bf048577c82b775b3e99cb0fec
m-miyawaki <miyawakifamilymyam@gmail.com>
2026-06-17 23:37:21 +0900

    feat(menu): add MenuGridItem component

---
+++ src/components/menu/MenuGridItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <div
+    class="menu-grid-item"
+    :class="{ 'menu-grid-item--clickable': !!item }"
+    @click="item && emit('click')"
+  >
+    <div class="menu-grid-item__tile" :class="{ 'menu-grid-item__tile--empty': !item }">
+      <v-icon v-if="item" :icon="item.icon" size="28" color="white" />
+    </div>
+    <span v-if="item" class="menu-grid-item__label">{{ item.label }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { MenuItem } from '@/stores/menuStore'
+
+defineProps<{ item?: MenuItem }>()
+const emit = defineEmits<{ click: [] }>()
+</script>
+
+<style scoped>
+.menu-grid-item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 6px;
+}
+.menu-grid-item--clickable {
+  cursor: pointer;
+}
+.menu-grid-item__tile {
+  width: 56px;
+  height: 56px;
+  background: rgb(var(--v-theme-primary));
+  border-radius: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.menu-grid-item__tile--empty {
+  background: transparent;
+  border: 2px dashed rgba(var(--v-theme-on-surface), 0.15);
+}
+.menu-grid-item__label {
+  font-size: 11px;
+  color: rgb(var(--v-theme-on-background));
+  text-align: center;
+  line-height: 1.3;
+  max-width: 64px;
+  word-break: break-all;
+}
+</style>
