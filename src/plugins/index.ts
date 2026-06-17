7505fd232053888903543f6d0533886aded78054
m-miyawaki <miyawakifamilymyam@gmail.com>
2026-06-17 23:35:45 +0900

    feat: register pinia-plugin-persistedstate

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,11 +1,14 @@
 import vuetify from './vuetify'
 import router from '@/router'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import type { App } from 'vue'
 
 export function registerPlugins(app: App) {
+  const pinia = createPinia()
+  pinia.use(piniaPluginPersistedstate)
   app
     .use(vuetify)
     .use(router)
-    .use(createPinia())
+    .use(pinia)
 }
