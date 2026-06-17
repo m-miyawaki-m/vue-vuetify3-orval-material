ebedcbc64f094a5cd93bf7676f8f91dc12af49fa
m-miyawaki <miyawakifamilymyam@gmail.com>
2026-06-17 23:54:06 +0900

    feat(home): add QuickScannerButton to home screen

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,10 +1,12 @@
 <template>
   <MainLayout title="VuetifyPoC">
     <MenuGrid />
+    <QuickScannerButton />
   </MainLayout>
 </template>
 
 <script setup lang="ts">
 import MainLayout from '@/components/layout/MainLayout.vue'
 import MenuGrid from '@/components/menu/MenuGrid.vue'
+import QuickScannerButton from '@/components/menu/QuickScannerButton.vue'
 </script>
