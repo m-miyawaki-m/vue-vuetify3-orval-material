6f5665af698921ff752c7b52ad2e7e7f28832a8d
m-miyawaki <miyawakifamilymyam@gmail.com>
2026-07-12 22:52:49 +0900

    refactor: Android の Room/Kotlin 構成を撤去（ScanRecordPlugin・db パッケージ・KSP ツールチェーン）

--- android/app/src/main/java/com/example/myapp/MainActivity.java
+++ android/app/src/main/java/com/example/myapp/MainActivity.java
@@ -7,7 +7,6 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(SampleSdkPlugin.class); // アプリ内ローカルプラグインは自動登録されないため super より前に登録
-        registerPlugin(ScanRecordPlugin.class);
         super.onCreate(savedInstanceState);
     }
 }
--- android/app/src/main/java/com/example/myapp/db/FeatureCount.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapp.db
-
-import androidx.room.ColumnInfo
-
-/** countDrafts の GROUP BY 結果1行 */
-data class FeatureCount(
-    @ColumnInfo(name = "feature_id") val featureId: String,
-    val cnt: Int,
-)
